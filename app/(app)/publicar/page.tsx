b75f503f5950acfb5f32791319fc54b2878bb715
Clarozzz <jcsdlg49@gmail.com>
2024-10-22 22:16:00 -0600

    v=0.4

--- app/(app)/publicar/page.tsx
+++ app/(app)/publicar/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import PublicarClient from './components/publicar'
+import Navbar from '@/components/navbar'
 
 export default function PagePublicar() {
   return (
-    <PublicarClient/>
+    <>
+      <Navbar />
+      <PublicarClient />
+    </>
   )
 }
--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
@@ -8,35 +9,40 @@ export default function Page() {
             <div className="px-80">
                 <h1 className="mt-16 text-6xl montserrat font-bold">Mi cuenta</h1>
                 <div className="flex justify-between gap-20 mt-16">
-                    <div className="w-1/4 bg-slate-300 rounded px-6 py-10">
-                        <div className="flex justify-center">
-                            <div>
-                                <Avatar className="w-24 h-24">
-                                    <AvatarImage src="https://github.com/shadcn.png" />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                                <h2 className="mt-4 text-xl font-bold text-center">
-                                    Anthony
-                                </h2>
+                    <div className="w-1/4">
+                        <div className="bg-slate-300 px-6 py-10 rounded">
+                            <div className="flex justify-center">
+                                <div>
+                                    <Avatar className="w-24 h-24">
+                                        <AvatarImage src="https://github.com/shadcn.png" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    <h2 className="mt-4 text-xl font-bold text-center">
+                                        Anthony
+                                    </h2>
+                                </div>
                             </div>
+                            <ul className="mt-8">
+                                <li>
+                                    Informacion de la cuenta
+                                </li>
+                                <li>
+                                    Historial de compras
+                                </li>
+                                <li>
+                                    Productos en venta
+                                </li>
+                            </ul>
                         </div>
-                        <ul className="mt-8">
-                            <li>
-                                Informacion de la cuenta
-                            </li>
-                            <li>
-                                Historial de compras
-                            </li>
-                            <li>
-                                Productos en venta
-                            </li>
-                        </ul>
                     </div>
                     <div className="w-3/4">
-                        <h2 className="font-bold">Informacion de la cuenta</h2>
+                        <h2 className="font-bold h-96">Informacion de la cuenta</h2>
+                        <h2 className="font-bold h-96">Informacion de la cuenta</h2>
+                        <h2 className="font-bold h-96">Informacion de la cuenta</h2>
                     </div>
                 </div>
             </div>
+            <Footer />
         </>
     )
 }
