8541590eb68f578defec70aa1893d8f5db45cfd5
Franklin Amador <joelamador55@gmail.com>
2024-11-06 00:47:33 -0600

    fixes a paginacion

--- app/productos/components/productos-vista.tsx
+++ app/productos/components/productos-vista.tsx
@@ -1,3 +1,93 @@
+// 'use client'
+
+// import { Button } from "@/components/ui/button";
+// import { Card, CardContent, CardFooter } from "@/components/ui/card";
+// import { motion } from "framer-motion";
+// import React, { useState, useEffect } from "react";
+// import Image from "next/image"
+// import { getProductos } from "@/app/api/productos";
+// import { usePathname, useRouter } from "next/navigation";
+// import Cargando from "@/components/ui/cargando";
+
+// export default function ProductosVista() {
+//   const [productos, setProductos] = useState<Producto[]>([]);
+//   const [productosSeleccion, setproductosSeleccion] = useState<Producto| null>(null)
+//   const pathname = usePathname()
+//   const router = useRouter()
+//   const [isPending, startTransition] = React.useTransition()
+
+//   const handleCardClick = (product: Producto) => {
+//     setproductosSeleccion(product)
+//     startTransition(() => {
+//       router.push(`${pathname}/${product.id}`)
+//     })
+//   }
+
+//   useEffect(() => {
+//     const obtenerProductos = async () => {
+//       try {
+//         const data = await getProductos();
+//         setProductos(data);
+//       } catch (error) {
+//         console.error("Error al obtener los productos:", error);
+//       }
+//     };
+
+//     obtenerProductos();
+//   }, []);
+
+//   return (  
+//       <div id="contenedor filtro y card" className="flex flex-row">
+//         {
+//         isPending && (
+//         <Cargando/>
+//         )
+//       }
+//         <div id="tarjetas" className="flex flex-col w-full justify-center items-center">
+//           <section className="py-14 bg-background">
+//             <div className="container mx-auto">
+//               <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-6">
+//                 {productos.map((product) => (
+//                   <motion.div key={product.id} onClick={() => { handleCardClick(product) }} className="cursor-pointer">
+//                     <Card>
+//                       <CardContent className="p-4">
+//                         <Image
+//                           src={product.productos_imagenes[0]?.url}
+//                           alt={product.nombre}
+//                           width={300}
+//                           height={300}
+//                           className="w-52 h-52 mb-4 rounded object-cover object-center"
+//                         />
+//                         <h3 className="text-lg font-semibold truncate max-w-40">{product.nombre}</h3>
+//                         <p className="text-gray-600">Lps. {product.precio.toFixed(2)}</p>
+//                         <p className="text-sm text-gray-500">Categoría: {product.productos_categorias[0]?.categorias.nombre}</p>
+//                         <p className="text-sm text-gray-500">Vendedor: {product.vendedores.usuarios.nombre}</p>
+//                       </CardContent>
+//                       <CardFooter>
+//                         <Button
+//                           onClick={() => { handleCardClick(product) }}
+//                           className={`w-full bg-custom-blue text-white hover:bg-blue-900 hover:text-white ${
+//                             productosSeleccion?.id ===  product.id ? 'ring-2 ring-ring ring-sec ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ' : ''
+
+//                           }`}
+//                           >
+//                           <p className="truncate max-w-32">Ver más detalles</p>
+//                         </Button>
+//                       </CardFooter>
+//                     </Card>
+//                   </motion.div>
+//                 ))}
+//               </div>
+//             </div>
+//           </section>
+//         </div>
+//       </div>
+//   );
+// }
+
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 'use client'
 
 import { Button } from "@/components/ui/button";
@@ -8,13 +98,17 @@ import Image from "next/image"
 import { getProductos } from "@/app/api/productos";
 import { usePathname, useRouter } from "next/navigation";
 import Cargando from "@/components/ui/cargando";
+import { Producto, ProductosResponse } from "../../global";
 
 export default function ProductosVista() {
   const [productos, setProductos] = useState<Producto[]>([]);
-  const [productosSeleccion, setproductosSeleccion] = useState<Producto| null>(null)
+  const [productosSeleccion, setproductosSeleccion] = useState<Producto | null>(null)
   const pathname = usePathname()
   const router = useRouter()
   const [isPending, startTransition] = React.useTransition()
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(8); // Valor inicial de 4 elementos por página
+  const [totalItems, setTotalItems] = useState<number>(0);
 
   const handleCardClick = (product: Producto) => {
     setproductosSeleccion(product)
@@ -23,64 +117,124 @@ export default function ProductosVista() {
     })
   }
 
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    fetchProductos(page, itemsPerPage);
+  }
+
+  const handleItemsPerPageChange = (value: number) => {
+    setItemsPerPage(value);
+    setCurrentPage(1); // Reiniciar la página a 1 cuando se cambia la cantidad de elementos por página
+    fetchProductos(1, value);
+  }
+
+  const fetchProductos = async (page: number, limit: number) => {
+    try {
+      const data: ProductosResponse = await getProductos(page, limit);
+      setProductos(data.items);
+      setTotalItems(data.total);
+    } catch (error) {
+      console.error("Error al obtener los productos:", error);
+    }
+  }
+
   useEffect(() => {
-    const obtenerProductos = async () => {
-      try {
-        const data = await getProductos();
-        setProductos(data);
-      } catch (error) {
-        console.error("Error al obtener los productos:", error);
-      }
-    };
+    fetchProductos(currentPage, itemsPerPage);
+  }, [currentPage, itemsPerPage]);
 
-    obtenerProductos();
-  }, []);
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-  return (  
-      <div id="contenedor filtro y card" className="flex flex-row">
-        {
+  return (
+    <div id="contenedor filtro y card" className="flex flex-row">
+      {
         isPending && (
-        <Cargando/>
+          <Cargando />
         )
       }
-        <div id="tarjetas" className="flex flex-col w-full justify-center items-center">
-          <section className="py-14 bg-background">
-            <div className="container mx-auto">
-              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-6">
-                {productos.map((product) => (
-                  <motion.div key={product.id} onClick={() => { handleCardClick(product) }} className="cursor-pointer">
-                    <Card>
-                      <CardContent className="p-4">
-                        <Image
-                          src={product.productos_imagenes[0]?.url}
-                          alt={product.nombre}
-                          width={300}
-                          height={300}
-                          className="w-52 h-52 mb-4 rounded object-cover object-center"
-                        />
-                        <h3 className="text-lg font-semibold truncate max-w-40">{product.nombre}</h3>
-                        <p className="text-gray-600">Lps. {product.precio.toFixed(2)}</p>
-                        <p className="text-sm text-gray-500">Categoría: {product.productos_categorias[0]?.categorias.nombre}</p>
-                        <p className="text-sm text-gray-500">Vendedor: {product.vendedores.usuarios.nombre}</p>
-                      </CardContent>
-                      <CardFooter>
-                        <Button
-                          onClick={() => { handleCardClick(product) }}
-                          className={`w-full bg-custom-blue text-white hover:bg-blue-900 hover:text-white ${
-                            productosSeleccion?.id ===  product.id ? 'ring-2 ring-ring ring-sec ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ' : ''
-
-                          }`}
-                          >
-                          <p className="truncate max-w-32">Ver más detalles</p>
-                        </Button>
-                      </CardFooter>
-                    </Card>
-                  </motion.div>
-                ))}
-              </div>
+      <div id="tarjetas" className="flex flex-col w-full justify-center items-center">
+        <section className="py-20 bg-background">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold text-center montserrat text-custom-blue mb-10">
+              Nuestros Productos Destacados
+            </h2>
+            {/* Selector de cantidad de elementos por página */}
+            <div className="mb-4 flex justify-end items-center">
+              <label htmlFor="items-per-page" className="mr-2">Elementos por página:</label>
+              <select
+                id="items-per-page"
+                value={itemsPerPage}
+                onChange={(e) => handleItemsPerPageChange(parseInt(e.target.value))}
+                className="bg-white border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-24"
+              >
+                <option value={4}>4</option>
+                <option value={8}>8</option>
+                <option value={12}>12</option>
+                <option value={16}>16</option>
+              </select>
+              <p className="ml-4 text-gray-600">Página {currentPage} de {totalPages}</p>
             </div>
-          </section>
-        </div>
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-6">
+              {productos.map((product) => (
+                <motion.div key={product.id} whileHover={{ scale: 1.05 }}>
+                  <Card>
+                    <CardContent className="p-4">
+                      <Image
+                        src={product.productos_imagenes[0]?.url}
+                        alt={product.nombre}
+                        width={300}
+                        height={300}
+                        className="w-full h-48 object-cover mb-4 rounded"
+                      />
+                      <h3 className="text-lg font-semibold">{product.nombre}</h3>
+                      <p className="text-gray-600">Lps. {product.precio.toFixed(2)}</p>
+                      <p className="text-sm text-gray-500">Categoría: {product.productos_categorias[0]?.categorias.nombre}</p>
+                      <p className="text-sm text-gray-500">Vendedor: {product.vendedores.usuarios.nombre}</p>
+                    </CardContent>
+                    <CardFooter>
+                      <Button
+                        onClick={() => { handleCardClick(product) }}
+                        className={`w-full bg-custom-blue text-white hover:bg-blue-900 hover:text-white ${
+                          productosSeleccion?.id === product.id ? 'ring-2 ring-ring ring-sec ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ' : ''
+                        }`}
+                      >
+                        <p className="truncate max-w-32">Ver más detalles</p>
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </motion.div>
+              ))}
+            </div>
+            {/* Paginación */}
+            <div className="flex justify-center mt-6 gap-2">
+              {currentPage > 1 && (
+                <Button
+                  onClick={() => handlePageChange(currentPage - 1)}
+                  className="px-3 py-2 leading-tight border border-gray-300 bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                >
+                  Anterior
+                </Button>
+              )}
+              {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+                <Button
+                  key={page}
+                  onClick={() => handlePageChange(page)}
+                  className={`px-3 py-2 leading-tight border border-gray-300 ${currentPage === page ? 'bg-blue-500 text-white' : 'bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700'}`}
+                >
+                  {page}
+                </Button>
+              ))}
+              {currentPage < totalPages && (
+                <Button
+                  onClick={() => handlePageChange(currentPage + 1)}
+                  className="px-3 py-2 leading-tight border border-gray-300 bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                >
+                  Siguiente
+                </Button>
+              )}
+            </div>
+          </div>
+        </section>
       </div>
+    </div>
   );
 }
