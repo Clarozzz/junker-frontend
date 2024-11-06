1ae90f8e4c1ae0b935d0f40a1beaabcb6077c494
Clarozzz <jcsdlg49@gmail.com>
2024-11-06 12:29:54 -0600

    Update publicar.tsx

--- app/(app)/publicar/components/publicar.tsx
+++ app/(app)/publicar/components/publicar.tsx
@@ -22,6 +22,7 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { getCategorias } from "@/app/api/categorias";
+import { Categoria } from "@/app/global";
 
 
 const productSchema = z.object({
