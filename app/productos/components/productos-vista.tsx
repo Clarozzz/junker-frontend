c9f9e4a700a54ea84d3107f1ff159e8cf967d20b
Franklin Amador <joelamador55@gmail.com>
2024-11-15 01:27:09 -0600

    filtros de productos fv1

--- app/productos/components/productos-vista.tsx
+++ app/productos/components/productos-vista.tsx
@@ -1,4 +1,3 @@
-// * v2 test
 
 "use client";
 
@@ -11,25 +10,22 @@ import { getProductos } from "@/app/api/productos";
 import { usePathname, useRouter } from "next/navigation";
 import Cargando from "@/components/ui/cargando";
 
-// Tipo para el producto, si no lo tienes definido puedes adaptarlo.
-
-interface ProductosVistaProps {
-  categoria: string | null; // Recibe la categoría como prop
-}
-
-export default function ProductosVista({ categoria }: ProductosVistaProps) {
-  const [productos, setProductos] = useState<Producto[]>([]);
-  const [productosSeleccion, setproductosSeleccion] = useState<Producto | null>(
-    null
-  );
+export default function ProductosVista({
+  categoria,
+  precio_min,
+  precio_max,
+  estado,
+}: ProductosVistaProps) {
+  const [productos, setProductos] = useState<ProductoVista[]>([]);
+  const [productosSeleccion, setproductosSeleccion] = useState<ProductoVista | null>(null);
   const pathname = usePathname();
   const router = useRouter();
   const [isPending, startTransition] = React.useTransition();
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [itemsPerPage, setItemsPerPage] = useState<number>(8); // Valor inicial de 4 elementos por página
+  const [itemsPerPage, setItemsPerPage] = useState<number>(8);
   const [totalItems, setTotalItems] = useState<number>(0);
 
-  const handleCardClick = (product: Producto) => {
+  const handleCardClick = (product: ProductoVista) => {
     setproductosSeleccion(product);
     startTransition(() => {
       router.push(`${pathname}/${product.id}`);
@@ -38,102 +34,98 @@ export default function ProductosVista({ categoria }: ProductosVistaProps) {
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
-    fetchProductos(page, itemsPerPage, categoria);
+    fetchProductos(page, itemsPerPage, categoria, precio_min, precio_max, estado);
   };
 
   const handleItemsPerPageChange = (value: number) => {
     setItemsPerPage(value);
     setCurrentPage(1);
-    fetchProductos(1, value, categoria);
+    fetchProductos(1, value, categoria, precio_min, precio_max, estado);
   };
 
   const fetchProductos = async (
     page: number,
     limit: number,
-    categoria: string | null
+    categoria: string | null,
+    precio_min: number | null,
+    precio_max: number | null,
+    estado: string | null
   ) => {
+
     try {
       const data: ProductosResponse = await getProductos(
         page,
         limit,
-        categoria
+        categoria,
+        precio_min,
+        precio_max,
+        estado
       );
-      // Filtramos los productos para excluir aquellos con categoría null
-      const filteredProducts = data.items.filter(
-        (product) =>
-          product.productos_categorias &&
-          product.productos_categorias[0]?.categorias !== null
-      );
-      setProductos(filteredProducts); // Asignamos los productos filtrados
-      setTotalItems(filteredProducts.length); // Actualizamos el total de productos filtrados
+
+
+      if (data && data.items) {
+        setProductos(data.items);
+        setTotalItems(data.total);
+      } else {
+        console.error("No se recibieron productos o hay un error en la estructura de datos.");
+      }
     } catch (error) {
       console.error("Error al obtener los productos:", error);
     }
   };
 
   useEffect(() => {
-    fetchProductos(currentPage, itemsPerPage, categoria);
-  }, [currentPage, itemsPerPage, categoria]);
+    fetchProductos(currentPage, itemsPerPage, categoria, precio_min, precio_max, estado);
+  }, [currentPage, itemsPerPage, categoria, precio_min, precio_max, estado]);
 
   const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   return (
     <div id="contenedor filtro y card" className="flex flex-row">
       {isPending && <Cargando />}
-      <div
-        id="tarjetas"
-        className="flex flex-col w-full justify-center items-center"
-      >
+      <div id="tarjetas" className="flex flex-col w-full justify-center items-center">
         <section className="py-14 bg-background">
           <div className="container mx-auto">
-            {/* Aquí removemos el selector de categoría ya que viene de SidebarProductos */}
-
-            {/* Mostrar los productos */}
             <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-6">
-              {productos.map((product) => (
-                <motion.div
-                  key={product.id}
-                  onClick={() => {
-                    handleCardClick(product);
-                  }}
-                  className="cursor-pointer"
-                >
-                  <Card>
-                    <CardContent className="p-4">
-                      <Image
-                        src={
-                          product.productos_imagenes[0]?.url ||
-                          "/default-image.jpg"
-                        } // Imagen por defecto si no existe
-                        alt={product.nombre}
-                        width={400}
-                        height={400}
-                        className="w-52 h-52 mb-4 rounded object-cover object-center"
-                      />
-                      <h3 className="text-lg font-semibold truncate max-w-40">
-                        {product.nombre}
-                      </h3>
-                      <p className="text-gray-600">
-                        Lps. {product.precio.toFixed(2)}
-                      </p>
-                    </CardContent>
-                    <CardFooter>
-                      <Button
-                        onClick={() => {
-                          handleCardClick(product);
-                        }}
-                        className={`w-full bg-custom-blue text-white hover:bg-blue-900 hover:text-white ${
-                          productosSeleccion?.id === product.id
-                            ? "ring-2 ring-ring ring-sec ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 "
-                            : ""
-                        }`}
-                      >
-                        <p className="truncate max-w-32">Ver más detalles</p>
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                </motion.div>
-              ))}
+              {productos.length > 0 ? (
+                productos.map((product) => (
+                  <motion.div
+                    key={product.id}
+                    onClick={() => handleCardClick(product)}
+                    className="cursor-pointer"
+                  >
+                    <Card>
+                      <CardContent className="p-4">
+                        <Image
+                          src={product.imagen_url || "/default-image.jpg"} // Usar imagen predeterminada si no hay URL
+                          alt={product.nombre}
+                          width={400}
+                          height={400}
+                          className="w-52 h-52 mb-4 rounded object-cover object-center"
+                        />
+                        <h3 className="text-lg font-semibold truncate max-w-40">
+                          {product.nombre}
+                        </h3>
+                        <p className="text-gray-600">{product.precio.toFixed(2)}</p>
+                      </CardContent>
+                      <CardFooter>
+                        <Button
+                          onClick={() => handleCardClick(product)}
+                          className={`w-full bg-custom-blue text-white hover:bg-blue-900 hover:text-white ${
+                            productosSeleccion?.id === product.id
+                              ? "ring-2 ring-ring ring-sec ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 "
+                              : ""
+                          }`}
+                        >
+                          <p className="truncate max-w-32">Ver más detalles</p>
+                        </Button>
+                      </CardFooter>
+                    </Card>
+                  </motion.div>
+                ))
+              ) : (
+                <p>No se encontraron productos.</p>
+              )}
             </div>
 
             {/* Paginación */}
@@ -146,21 +138,19 @@ export default function ProductosVista({ categoria }: ProductosVistaProps) {
                   Anterior
                 </Button>
               )}
-              {Array.from({ length: totalPages }, (_, i) => i + 1).map(
-                (page) => (
-                  <Button
-                    key={page}
-                    onClick={() => handlePageChange(page)}
-                    className={`px-3 py-2 leading-tight border border-gray-300 ${
-                      currentPage === page
-                        ? "bg-blue-500 text-white"
-                        : "bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                    }`}
-                  >
-                    {page}
-                  </Button>
-                )
-              )}
+              {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+                <Button
+                  key={page}
+                  onClick={() => handlePageChange(page)}
+                  className={`px-3 py-2 leading-tight border border-gray-300 ${
+                    currentPage === page
+                      ? "bg-blue-500 text-white"
+                      : "bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                  }`}
+                >
+                  {page}
+                </Button>
+              ))}
               {currentPage < totalPages && (
                 <Button
                   onClick={() => handlePageChange(currentPage + 1)}
@@ -196,3 +186,4 @@ export default function ProductosVista({ categoria }: ProductosVistaProps) {
     </div>
   );
 }
+
