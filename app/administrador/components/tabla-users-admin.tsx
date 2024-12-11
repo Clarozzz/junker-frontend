5ff541ea11d5024a032b5e2283ca2fee8e8267c7
Clarozzz <jcsdlg49@gmail.com>
2024-12-10 22:34:02 -0600

    a

--- app/administrador/components/tabla-users-admin.tsx
+++ app/administrador/components/tabla-users-admin.tsx
@@ -14,55 +14,55 @@ import ModalAgregarRol from "./modal-agregar-rol";
 import { SearchInput } from "@/components/ui/search-input";
 
 
-export default function DataTableAdmin () {
-    const [usuarios, setUsuarios] = useState<UsuarioVista[]>([]);
-    const pathname = usePathname();
-    const [currentPage, setCurrentPage] = useState<number>(1);
-    const [itemsPerPage, setItemsPerPage] = useState<number>(8);
-    const [totalItems, setTotalItems] = useState<number>(0);
-    const [totalPages, setTotalPages] = useState<number>(0);
+export default function DataTableAdmin() {
+  const [usuarios, setUsuarios] = useState<UsuarioVista[]>([]);
+  const pathname = usePathname();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(8);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
-    const [searchQuery, setSearchQuery] = useState<string>('');
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
-    const [personaSeleccionada, setPersonaSeleccionada] =
+  const [personaSeleccionada, setPersonaSeleccionada] =
     useState<UsuarioVista | null>(null)
 
 
-    const fetchUsuarios = useCallback(async () => {
-      try {
-        const data: UsuariosResponse = await getUsuarios(
-          currentPage,
-          itemsPerPage,
-          null,
-          null,
-          null,
-          "administrador",
-          searchQuery,
-          null
-        );
-    
-        if (data && data.items) {
-          setUsuarios(data.items);
-          setTotalItems(data.total);
-          setTotalPages(data.total_pages);  
-        }
-      } catch (error) {
-        console.error("Error al obtener los usuarios:", error);
-        setUsuarios([]);
-        setTotalItems(0);
-        setTotalPages(0);
+  const fetchUsuarios = useCallback(async () => {
+    try {
+      const data: UsuariosResponse = await getUsuarios(
+        currentPage,
+        itemsPerPage,
+        null,
+        null,
+        null,
+        "administrador",
+        searchQuery,
+        null
+      );
+
+      if (data && data.items) {
+        setUsuarios(data.items);
+        setTotalItems(data.total);
+        setTotalPages(data.total_pages);
       }
-    }, [
-      currentPage,
-      itemsPerPage,
-      searchQuery,
-    ]);
-    
-    // Add a useEffect to watch for searchQuery changes
-    useEffect(() => {
-      setCurrentPage(1);
-      fetchUsuarios();
-    }, [searchQuery, fetchUsuarios]);
+    } catch (error) {
+      console.error("Error al obtener los usuarios:", error);
+      setUsuarios([]);
+      setTotalItems(0);
+      setTotalPages(0);
+    }
+  }, [
+    currentPage,
+    itemsPerPage,
+    searchQuery,
+  ]);
+
+  // Add a useEffect to watch for searchQuery changes
+  useEffect(() => {
+    setCurrentPage(1);
+    fetchUsuarios();
+  }, [searchQuery, fetchUsuarios]);
 
   useEffect(() => {
     setCurrentPage(1);
@@ -72,10 +72,10 @@ export default function DataTableAdmin () {
     fetchUsuarios();
   }, [fetchUsuarios]);
 
- 
+
   const handleItemsPerPageChange = (newItemsPerPage: number) => {
     setItemsPerPage(newItemsPerPage);
-    setCurrentPage(1);  
+    setCurrentPage(1);
   };
 
   const handlePageChange = (newPage: number) => {
@@ -99,15 +99,15 @@ export default function DataTableAdmin () {
   return (
     <div className='flex flex-col my-4'>
       <div className='mb-4 flex items-center'>
-                <div className='relative w-full max-w-md'>
-                <SearchInput
+        <div className='relative w-full max-w-md'>
+          <SearchInput
             placeholders={placeholders}
             onChange={handleChange}
             onSubmit={onSubmit}
             debounce={300}
           />
-                </div>
-            </div>
+        </div>
+      </div>
       <div className='-m-1.5 overflow-x-auto'>
         <div className='p-1.5 min-w-full inline-block align-middle'>
           <div className='border rounded-lg overflow-hidden dark:border-gray-700'>
@@ -216,17 +216,16 @@ export default function DataTableAdmin () {
                       <TableCell className='whitespace-nowrap px-3 py-3'>
                         <span
                           className={`capitalize px-2 py-1 rounded-md
-                          ${
-                            usuario.estado === 'activo'
+                          ${usuario.estado === 'activo'
                               ? 'bg-green-100 dark:bg-green-900/30 text-green-800 dark:text-green-500'
                               : usuario.estado === 'inactivo'
-                              ? 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900/30 dark:text-yellow-500'
-                              : usuario.estado === 'no disponible'
-                              ? 'bg-gray-100 text-gray-800 dark:bg-gray-900/30 dark:text-gray-500'
-                              : usuario.estado === 'cancelado'
-                              ? 'bg-red-100 text-red-800 dark:bg-red-900/30 dark:text-red-500'
-                              : 'bg-neutral-100 text-neutral-800 dark:bg-neutral-900/30 dark:text-neutral-500'
-                          }
+                                ? 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900/30 dark:text-yellow-500'
+                                : usuario.estado === 'no disponible'
+                                  ? 'bg-gray-100 text-gray-800 dark:bg-gray-900/30 dark:text-gray-500'
+                                  : usuario.estado === 'cancelado'
+                                    ? 'bg-red-100 text-red-800 dark:bg-red-900/30 dark:text-red-500'
+                                    : 'bg-neutral-100 text-neutral-800 dark:bg-neutral-900/30 dark:text-neutral-500'
+                            }
                         `}
                         >
                           {usuario.estado ?? 'No disponible'}
@@ -258,12 +257,11 @@ export default function DataTableAdmin () {
                             </Link>
                           </Button>
 
-                            <>
-                              <ModalAgregarRol
-                                usuario={personaSeleccionada}
-                              />
-
-                            </>
+                          <>
+                            {personaSeleccionada && (
+                              <ModalAgregarRol usuario={personaSeleccionada} />
+                            )}
+                          </>
 
                         </ButtonMoreDataTable>
                       </TableCell>
@@ -274,16 +272,16 @@ export default function DataTableAdmin () {
             </Table>
             {usuarios.length > 0 && (
               <div className="my-2 flex w-full justify-center">
-                <Pagination 
-                    totalPages={totalPages} 
-                    currentPage={currentPage}
-                    onPageChange={handlePageChange}
-                    totalItems={totalItems}  
-                    itemsPerPage={itemsPerPage} 
-                    onItemsPerPageChange={handleItemsPerPageChange} 
+                <Pagination
+                  totalPages={totalPages}
+                  currentPage={currentPage}
+                  onPageChange={handlePageChange}
+                  totalItems={totalItems}
+                  itemsPerPage={itemsPerPage}
+                  onItemsPerPageChange={handleItemsPerPageChange}
                 />
               </div>
-          )}                 
+            )}
           </div>
         </div>
       </div>
